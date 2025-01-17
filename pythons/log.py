4d0e1043b4f5da38144801e8fce4d501ca54c723
AxelPore <axel.pore@gmail.com>
2025-01-17 15:25:14 +0100

    final_commit

--- pythons/log.py
+++ pythons/log.py
@@ -1,4 +1,44 @@
 import logging
-#ikebfofe
-#epef
-#idbfibvribv
+
+logger = logging.getLogger("logs")
+logger.setLevel(10)
+fmt = "%(levelname)8s"
+fmt2 = "%(asctime)s %(levelname)8s %(message)s"
+class CustomFormatter(logging.Formatter):
+
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+    white = '\x1b[38;5;255m'
+
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: "%(asctime)s" + self.grey + self.fmt + self.reset + " %(message)s",
+            logging.INFO: "%(asctime)s" + self.white + self.fmt + self.reset + " %(message)s",
+            logging.WARNING: "%(asctime)s" + self.yellow + self.fmt + self.reset + " %(message)s",
+            logging.ERROR: "%(asctime)s" + self.red + self.fmt + self.reset + " %(message)s",
+            logging.CRITICAL: "%(asctime)s" + self.bold_red + self.fmt + self.reset + " %(message)s",
+            
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+    
+console_handler = logging.StreamHandler()
+console_handler.setLevel(10)
+console_handler.setFormatter(CustomFormatter(fmt))
+
+file_handler = logging.FileHandler("/var/log/bs_server/bs_server.log", mode="a", encoding="utf-8")
+file_handler.setLevel(10)
+file_handler.setFormatter(logging.Formatter(fmt2))
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
