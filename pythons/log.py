e646d9d00665ffa46e82e18ea0b4b7af996fba03
AxelPore <axel.pore@gmail.com>
2025-01-17 16:50:01 +0100

    final_commit

--- pythons/log.py
+++ pythons/log.py
@@ -36,7 +36,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(10)
 console_handler.setFormatter(CustomFormatter(fmt))
 
-file_handler = logging.FileHandler("../ids.log", mode="a", encoding="utf-8")
+file_handler = logging.FileHandler("/var/log/ids/ids.log", mode="a", encoding="utf-8")
 file_handler.setLevel(10)
 file_handler.setFormatter(logging.Formatter(fmt2))
 
